866928f690e895b5b13b03bec9a872378839ded7
deuxPoulpe <yayafeu2@gmail.com>
2022-05-28 08:57:22 +0200

    upload v2 icoop

--- js/script.js
+++ js/script.js
@@ -1,14 +1,48 @@
-function toggleMenu() {
-  const navbar = document.querySelector(".navbar");
-  const burger = document.querySelector(".burger");
-  burger.addEventListener("click", (e) => {
-    navbar.classList.toggle("show-nav");
+function sendURL(url) {
+  urljson = JSON.stringify(ulrt);
+  promesse = fetch("http://localhost/index.php/sendURL", {
+    method: "POST",
+    body: urljson,
   });
+  window.location.reload();
+  promesse
+    .then((r) => r.json())
+    .then((data) => {
+      recover(data);
+    });
+  console.log(url);
 }
-toggleMenu();
 
-const loader = document.querySelector(".loader");
+function recover(int) {
+  int.innerHTML;
+}
 
-window.addEventListener("load", () => {
-  loader.classList.add("fondu-out");
-});
+function pause(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function afficherDate() {
+  while (true) {
+    await pause(1000);
+    var cejour = new Date();
+    var options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "2-digit",
+    };
+    var date = cejour.toLocaleDateString("fr-FR", options);
+    var heure =
+      ("0" + cejour.getHours()).slice(-2) +
+      ":" +
+      ("0" + cejour.getMinutes()).slice(-2) +
+      ":" +
+      ("0" + cejour.getSeconds()).slice(-2);
+    var dateheure = date + " " + heure;
+    var dateheure = dateheure.replace(/(^\w{1})|(\s+\w{1})/g, (lettre) =>
+      lettre.toUpperCase()
+    );
+    document.getElementById("dateheure").innerHTML = dateheure;
+  }
+}
+afficherDate();
